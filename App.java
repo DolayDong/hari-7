bcbee0e1d0fd7e033968f320934390d9398f4a6a
Sapto Aji <saosabc321@gmail.com>
2019-12-10 18:10:33 +0700

    tambah segitiga

--- App.java
+++ App.java
@@ -10,21 +10,55 @@ public class App{
         Mobil alprad = new Mobil();
               alprad.merk = "Alprad";
               alprad.nyalainMesin();
-              alprad.maju();     
+              alprad.maju();
 
+        Segitiga segitigaSikuSiku = new Segitiga();
+              segitigaSikuSiku.jenis = "Siku Siku ";
+              segitigaSikuSiku.alas = 10;
+              segitigaSikuSiku.tinggi = 15;
+              segitigaSikuSiku.hitungLuas();
+
+        Segitiga segitigaSamaKaki = new Segitiga();
+            segitigaSamaKaki.jenis = "Sama Kaki ";
+            segitigaSamaKaki.alas = 2;
+            segitigaSamaKaki.tinggi = 10;
+            segitigaSamaKaki.hitungLuas();
     }
 }
 
 class Mobil{
+
+    // atribut / property
     String merk;
     int jumlahRoda;
     String warna;
+
+    // method
 
     public void maju(){
         System.out.println("ini Adalah Mobil " + this.merk + ", Mobil ini mau Maju nih");
     }
 
+    // method / perilaku
+
     public void nyalainMesin() {
         System.out.println("Mesin Nyala, Mbreemmmmmm ");
+    }
+
+}
+
+class Segitiga{
+
+    // atribut / property
+    String jenis;
+    int alas;
+    int tinggi;
+
+
+    // method / perilaku
+    public void hitungLuas(){
+        double luas = this.alas * this.tinggi / 2;
+
+        System.out.println("Luas " + this.jenis + luas);
     }
 }
