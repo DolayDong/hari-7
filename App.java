da290142dd4507dd1a6286794311faa18e3715f3
Sapto Aji <saosabc321@gmail.com>
2019-12-10 17:38:22 +0700

    program pertama

--- App.java
+++ App.java
@@ -1,5 +1,30 @@
 public class App{
     public static void main(String[] args) {
         System.out.println("Welcome to Mobile Legend");
+
+        Mobil japero = new Mobil();
+              japero.merk = "Japero";
+              japero.nyalainMesin();
+              japero.maju();
+
+        Mobil alprad = new Mobil();
+              alprad.merk = "Alprad";
+              alprad.nyalainMesin();
+              alprad.maju();     
+
+    }
+}
+
+class Mobil{
+    String merk;
+    int jumlahRoda;
+    String warna;
+
+    public void maju(){
+        System.out.println("ini Adalah Mobil " + this.merk + ", Mobil ini mau Maju nih");
+    }
+
+    public void nyalainMesin() {
+        System.out.println("Mesin Nyala, Mbreemmmmmm ");
     }
 }
